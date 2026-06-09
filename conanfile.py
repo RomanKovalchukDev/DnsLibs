cd1c5ca714f5b0b9b53170ee56f573e96aa362e1
Roman Kovalchuk <romankovalchuk@nerdzlab.com>
2026-06-09 11:19:42 +0300

    chore: bump native_libs_common ref to v8.1.33-cira1

--- conanfile.py
+++ conanfile.py
@@ -39,7 +39,7 @@ class DnsLibsConan(ConanFile):
         self.requires("klib/2021-04-06@adguard_team/native_libs_common", transitive_headers=True)
         self.requires("ldns/2024-06-20@adguard_team/native_libs_common", transitive_headers=True)
         self.requires("magic_enum/0.9.5", transitive_headers=True)
-        self.requires("native_libs_common/v8.1.33-cira@adguard_team/native_libs_common", transitive_headers=True)
+        self.requires("native_libs_common/v8.1.33-cira1@adguard_team/native_libs_common", transitive_headers=True)
         self.requires("ngtcp2/1.0.1@adguard_team/native_libs_common", transitive_headers=True)
         self.requires("pcre2/10.37@adguard_team/native_libs_common", transitive_headers=True)
         self.requires("tldregistry/2022-12-26@adguard_team/native_libs_common", transitive_headers=True)
